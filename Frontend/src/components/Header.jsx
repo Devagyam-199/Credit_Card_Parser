0156ce8ebc06d02efdfc93d3aa56b579e64b9bcc
Devagya Mistry <mrx28sep2002@gmail.com>
2025-10-23 14:17:43 +0530

    credit card parser 2.0

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
               </div>
             </div>
             <span className="text-2xl sm:text-3xl font-bold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent animate-pulse-slow">
-              StatementAI
+              CrediWiz
             </span>
           </Link>
           
